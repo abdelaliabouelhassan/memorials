63e27e4bb04f69d508e3e14a938970a237171f77
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-12-29 18:27:10 +0100

    profile details

--- resources/js/components/pages/profile/index.vue
+++ resources/js/components/pages/profile/index.vue
@@ -28,36 +28,15 @@
                           <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
                         </svg>
                     </button>
-                    <button>
+                    <router-link to="/profile/1">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 text-primary hover:text-opacity-65 duration-200">
                           <path fill-rule="evenodd" d="M15.75 2.25H21a.75.75 0 0 1 .75.75v5.25a.75.75 0 0 1-1.5 0V4.81L8.03 17.03a.75.75 0 0 1-1.06-1.06L19.19 3.75h-3.44a.75.75 0 0 1 0-1.5Zm-10.5 4.5a1.5 1.5 0 0 0-1.5 1.5v10.5a1.5 1.5 0 0 0 1.5 1.5h10.5a1.5 1.5 0 0 0 1.5-1.5V10.5a.75.75 0 0 1 1.5 0v8.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V8.25a3 3 0 0 1 3-3h8.25a.75.75 0 0 1 0 1.5H5.25Z" clip-rule="evenodd" />
                         </svg>
 
-                    </button>
+                    </router-link>
                 </div>
             </div>
-             <div class=" w-full bg-[#F2F2F1] rounded-md flex items-center justify-between px-5 shadow py-7">
-                <div class=" flex items-center space-x-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 text-primary">
-                      <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
-                    </svg>
-                    <span class=" text-primary font-Altivo text-xl font-normal">PROFILENAME</span>
-
-                </div>
-                 <div class=" flex items-center space-x-2">
-                    <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 text-primary hover:text-opacity-65 duration-200">
-                          <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32L19.513 8.2Z" />
-                        </svg>
-                    </button>
-                    <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 text-primary hover:text-opacity-65 duration-200">
-                          <path fill-rule="evenodd" d="M15.75 2.25H21a.75.75 0 0 1 .75.75v5.25a.75.75 0 0 1-1.5 0V4.81L8.03 17.03a.75.75 0 0 1-1.06-1.06L19.19 3.75h-3.44a.75.75 0 0 1 0-1.5Zm-10.5 4.5a1.5 1.5 0 0 0-1.5 1.5v10.5a1.5 1.5 0 0 0 1.5 1.5h10.5a1.5 1.5 0 0 0 1.5-1.5V10.5a.75.75 0 0 1 1.5 0v8.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V8.25a3 3 0 0 1 3-3h8.25a.75.75 0 0 1 0 1.5H5.25Z" clip-rule="evenodd" />
-                        </svg>
-
-                    </button>
-                </div>
-            </div>
+            
         </div>
     </div>
   </MainLayouts>
--- resources/js/components/partials/UI/BottomSheet.vue
+++ resources/js/components/partials/UI/BottomSheet.vue
@@ -6,7 +6,7 @@
             </button>
             <div @click="openSheet" class=" w-full flex space-x-2 items-center cursor-pointer">
                 <div class=" w-14 h-14 rounded-full border  overflow-hidden">
-
+                    <img  src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" class=" w-full h-full object-cover" alt="">
                 </div>
                 <div class=" flex flex-col items-start -space-y-1">
                     <span class=" text-white uppercase font-Altivo text-xl font-medium">MAX MUSTERMANN</span>
@@ -15,10 +15,15 @@
             </div>
 
             <div class=" absolute bottom-4 right-4 space-x-2">
-                <button v-if="!open">
+                <button v-if="!open" @click="open = true; openType = 'comments'">
                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 text-white">
                         <path d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 0 0-1.032-.211 50.89 50.89 0 0 0-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 0 0 2.433 3.984L7.28 21.53A.75.75 0 0 1 6 21v-4.03a48.527 48.527 0 0 1-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979Z" />
                         <path d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 0 0 1.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0 0 15.75 7.5Z" />
+                    </svg>
+                </button>
+                <button v-if="openType === 'comments' && open" @click="openType = 'addcomment'">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-white hover:text-opacity-75 duration-200">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
                 </button>
                 <button>
@@ -30,8 +35,8 @@
         </div>
 
      <div class=" w-full h-full px-4" v-if="open">
-       <div class=" w-full h-[70%] md:h-[80%] overflow-y-auto bg-red-400">
-         <div class=" w-full p-4 space-y-4">
+       <div class=" w-full h-[70%] md:h-[80%] overflow-y-auto ">
+         <div class=" w-full p-4 space-y-4" v-if="openType === 'details'">
             <p class=" text-white text-lg font-Altivo font-normal">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos reprehenderit quasi iusto iste, vero dolores,
                 pariatur odio hic rem quod provident praesentium blanditiis, voluptatibus velit? Est quidem ipsa quo doloribus!
@@ -42,17 +47,62 @@
                 pariatur odio hic rem quod provident praesentium blanditiis, voluptatibus velit? Est quidem ipsa quo doloribus!
             </p>
         </div>
+        <div class=" w-full flex flex-col items-start space-y-4" v-else-if="openType === 'comments'">
+            
+                <div v-for="n in 10" :key="n" class="flex items-start gap-2.5">
+                <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="Jese image">
+                <div class=" w-full max-w-[320px] flex items-start space-y-2 flex-col">
+                    <div class="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-[#565352] bg-opacity-30  rounded-e-xl rounded-es-xl ">
+                    <div class="flex items-center space-x-2 rtl:space-x-reverse">
+                        <span class="text-sm  text-white font-Altivo ">Bonnie Green</span>
+                        <span class="text-sm font-normal text-gray-300 ">11:46</span>
+                    </div>
+                    <p class="text-sm font-normal py-2.5 text-white ">That's awesome. I think our users will really appreciate the improvements.</p>
+                
+                </div>
+                    <div class=" w-full flex items-center space-x-2">
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                            </svg>
+                        </button>
+                        <span class=" text-white text-sm font-Altivo">1</span>
+                    </div>
+                </div>
+                
+                </div>
+                
+
+        </div>
+        <div class=" w-full" v-else>
+            <div class=" w-full">
+                  <div class=" w-full pt-10">
+                        <div class="relative w-full min-w-[200px]">
+                            <textarea
+                            class="peer h-full text-white text-lg min-h-[100px] w-full resize-none rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-2.5 font-sans  font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
+                            placeholder=" "></textarea>
+                            <label
+                            class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                             Comment
+                            </label>
+                        </div>
+                   </div>
+                    <div class=" w-full py-4 px-4">
+                      <button class=" uppercase text-white bg-primary rounded-md hover:scale-y-110 duration-200 w-full py-2 font-Altivo text-lg">Create</button>
+                </div>
+            </div>
+        </div>
        </div>
 
        <div class=" w-full  flex">
         <div class=" flex items-center pt-3 sm:pt-5 space-x-4 m-auto">
-            <button>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 text-white">
+            <button @click="openType = 'details'">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10  duration-200" :class="{' text-[#5C5A5A]':openType != 'details','text-white':openType == 'details'}">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
                 </svg>
             </button>
-             <button>
-                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-10 h-10 text-white">
+             <button @click="openType = 'comments'">
+                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-10 h-10  duration-200" :class="{' text-[#5C5A5A]':openType != 'comments','text-white':openType == 'comments'}">
                         <path d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 0 0-1.032-.211 50.89 50.89 0 0 0-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 0 0 2.433 3.984L7.28 21.53A.75.75 0 0 1 6 21v-4.03a48.527 48.527 0 0 1-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979Z" />
                         <path d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 0 0 1.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0 0 15.75 7.5Z" />
                     </svg>
@@ -65,10 +115,31 @@
 
 
 <script setup>
-import { ref } from "vue";
+ import { ref } from "vue";
+ const openType = ref('details')
 
  const open = ref(false)
  const openSheet = () => {
     open.value = !open.value
  }
 </script>
+
+
+<style scoped>
+::-webkit-scrollbar {
+  width: 5px; /* Width of the scrollbar */
+}
+
+::-webkit-scrollbar-track {
+  background-color: #a7a7a758; /* Color of the track */
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: white; /* Color of the thumb */
+  border-radius: 6px; /* Rounded corners of the thumb */
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background-color: #555; /* Color of the thumb on hover */
+}
+</style>
