b816e59d167200ddb3fbfe6fab637e136e4176fa
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2024-01-03 17:32:17 +0100

    desktop fixes and log out

--- resources/js/components/partials/Header.vue
+++ resources/js/components/partials/Header.vue
@@ -28,7 +28,7 @@
             </button>
         </div>
 
-        <div class=" flex items-center justify-between">
+        <div class=" md:flex items-center justify-between hidden">
             <div class=" flex items-center space-x-8 text-primary font-Altivo text-lg">
                  <router-link to="/" class=" text-primary text-sm font-Altivo">
                      <img src="/images/LOGO_BLUE.png" class=" max-w-[12rem] object-cover" alt="logo blue">
@@ -37,15 +37,20 @@
                     <router-link to="/">KONTAKT</router-link>
                     <router-link to="/">HILFE</router-link>
                     <router-link to="/">SHOP</router-link>
-                    <router-link to="/" class=" text-base">PRIVACY POLICY</router-link>
-                    <router-link to="/" class=" text-base">IMPRINT</router-link>
             </div>
              <div>
-                <router-link to="/profile">
+             <div class=" flex items-center space-x-2">
+                   <router-link to="/profile">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-9 h-9 md:text-primary" :class="{' text-white':theme == 'dark'}">
                             <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
                             </svg>
                 </router-link>
+                 <button @click="logOut" v-if="store.user.email" class="  text-primary  flex hover:scale-y-110 duration-200">
+                        <svg xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 m-auto  h-10">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
+                    </svg>
+                </button>
+             </div>
             </div>
         </div>
 
@@ -58,11 +63,27 @@
 
 <script setup>
  import { defineEmits, ref } from "vue";
+ import { useStore } from "@/stores/states.js"
  const props = defineProps(['theme'])
  const open = ref(false)
+  
+
+ const store = useStore()
+ import axios from 'axios'
  const emit = defineEmits('openMenu')
  const openMenu = () =>{
     open.value = !open.value
     emit('openMenu')
  }
+
+  const logOut = () => {
+    axios.post('/api/logout').then((res) => {
+        localStorage.removeItem('token');
+        // Reset the axios Authorization header:
+        axios.defaults.headers.common['Authorization'] = 'Bearer';
+        store.user = [];
+        // Redirect the user to the login page:
+        window.location.href = '/'
+    })
+  }
 </script>
