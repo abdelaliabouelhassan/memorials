e838362e7b8275f57cd146dc7266c5b37096bc96
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2024-01-08 13:28:51 +0100

    index

--- resources/js/components/pages/index.vue
+++ resources/js/components/pages/index.vue
@@ -3,20 +3,18 @@
     <div class=" w-full h-full flex py-4 px-4">
         <div class=" m-auto space-y-4">
            <div >
-             <h1 class=" text-2xl font-Altivo text-primary font-medium">UNVERGESSLICHE MOMENTE,</h1>
-            <span  class=" text-xl font-Altivo text-primary font-normal">EWIGE ERINNERUNGEN</span>
+             <h1 class=" text-2xl font-Altivo text-primary font-medium uppercase">{{$t('unforgettable_moments')}},</h1>
+            <span  class=" text-xl font-Altivo text-primary font-normal uppercase">{{$t('eternal_memories')}}</span>
            </div>
 
-            <p class=" text-sm font-Altivo font-normal text-black max-w-xs">
-              LOG IN TO MANAGE YOUR SOULSTONES
-              OR BUY A SOUL MIRROR TO PRESERVE THE
-              MEMORIES OF YOUR LOVED ONES.
+            <p class=" text-sm uppercase font-Altivo font-normal text-black max-w-xs">
+              {{$t('login_to_manage_text')}}
             </p>
 
             <div class=" w-full flex flex-col items-start space-y-2 pt-10">
-              <button class=" uppercase text-white bg-primary rounded-md hover:scale-x-110 duration-200 w-full py-2 font-Altivo text-lg">go to shop</button>
+              <button class=" uppercase text-white bg-primary rounded-md hover:scale-x-110 duration-200 w-full py-2 font-Altivo text-lg">{{$t('go_to_shop')}}</button>
                <router-link to="/login" class=" w-full">
-                   <button class=" uppercase bg-white border-2 border-primary text-primary rounded-md hover:scale-x-110 duration-200 w-full py-2 font-Altivo text-lg">{{store.user.email ? 'my profiles' : 'log in'}}</button>
+                   <button class=" uppercase bg-white border-2 border-primary text-primary rounded-md hover:scale-x-110 duration-200 w-full py-2 font-Altivo text-lg">{{store.user.email ? 'my_profiles' : $t('login')}}</button>
                </router-link>
               
             </div>
