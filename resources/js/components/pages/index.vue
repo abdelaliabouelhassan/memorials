fe6febf6753124ce57af229b3a1eb34eaa39fe3a
NVOYED <l@rogotz.de>
2024-02-07 12:44:42 +0100

    link to shop fix

--- resources/js/components/pages/index.vue
+++ resources/js/components/pages/index.vue
@@ -12,10 +12,12 @@
             </p>
 
             <div class=" w-full flex flex-col items-start space-y-2 pt-10">
-              <button class=" uppercase text-white bg-primary rounded-md hover:scale-x-110 duration-200 w-full py-2 font-Altivo text-lg">{{$t('go_to_shop')}}</button>
-               <router-link to="/login" class=" w-full">
-                   <button class=" uppercase bg-white border-2 border-primary text-primary rounded-md hover:scale-x-110 duration-200 w-full py-2 font-Altivo text-lg">{{store.user.email ? $t('my_profiles') : $t('login')}}</button>
-               </router-link>
+              <a href="https://seelenstein.de/products/der-seelenstein" target="_blank" class=" w-full">
+                <button class=" uppercase text-white bg-primary rounded-md hover:scale-x-110 duration-200 w-full py-2 font-Altivo text-lg">{{$t('go_to_shop')}}</button>
+              </a>
+              <router-link to="/login" class=" w-full">
+                 <button class=" uppercase bg-white border-2 border-primary text-primary rounded-md hover:scale-x-110 duration-200 w-full py-2 font-Altivo text-lg">{{store.user.email ? $t('my_profiles') : $t('login')}}</button>
+              </router-link>
               
             </div>
         </div>
